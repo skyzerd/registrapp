5682fa963ab00d79471bf33eb12adebb541b3536
skyzerd <135839146+skyzerd@users.noreply.github.com>
2023-11-28 17:05:06 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+
+
+export const environment = {
+  production: true,
+  supabaseUrl: "https://tphmmjvjzvqrmuqmqlgn.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwaG1tanZqenZxcm11cW1xbGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2NDE1MzcsImV4cCI6MjAxMjIxNzUzN30.qh1fDYmecMxLOMOT_g0kKUcbsIOnvDjoPjy3T9UHpI0",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: "https://tphmmjvjzvqrmuqmqlgn.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwaG1tanZqenZxcm11cW1xbGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY2NDE1MzcsImV4cCI6MjAxMjIxNzUzN30.qh1fDYmecMxLOMOT_g0kKUcbsIOnvDjoPjy3T9UHpI0",
+};
+
